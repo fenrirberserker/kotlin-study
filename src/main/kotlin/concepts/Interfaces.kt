9c6819634debbac54140891948d2e919913ea636
fenrir <mlex.alba@gmail.com>
2023-05-30 15:10:56 -0600

    Interfaces

---
+++ src/main/kotlin/concepts/Interfaces.kt
@@ -0,0 +1,31 @@
+package concepts
+
+interface Time{
+
+    //regular method
+    fun setTime(hours: Int, mins: Int = 0, secs: Int = 0)
+    //default method
+    fun setTime(time: KevinTime) = setTime(time.hours)
+}
+
+interface EndOfTheWorld{
+    fun setTime(time: KevinTime){}
+}
+
+class KevinTime{
+    var hours: Int=0
+    var minutes: Int=0
+    var seconds: Int=0
+
+}
+
+class YetiTime: Time, EndOfTheWorld{
+    override fun setTime(time: KevinTime){
+        super<Time>.setTime(time)
+        super<EndOfTheWorld>.setTime(time)
+    }
+    override fun setTime(hours: Int, mins: Int, secs: Int) {
+
+    }
+
+}
